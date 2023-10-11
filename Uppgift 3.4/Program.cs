099d2cc31135e27b36da43dbfe1345666df37aa5
KraftaN <63770425+KrftaN@users.noreply.github.com>
2023-10-11 10:40:44 +0200

    Add project files.

---
+++ Uppgift 3.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hur lång är låten?");
+        string input = Console.ReadLine();
+        int mellanSlagIndex = input.IndexOf(" ");
+        int min = int.Parse(input[..mellanSlagIndex]);
+        int sek = int.Parse(input[(mellanSlagIndex + 1)..]);
+        int totalSek = (min * 60) + sek;
+
+        if (totalSek > 165 && totalSek < 260)
+        {
+            Console.WriteLine("Låten får spelas!");
+        }
+        else
+        {
+            Console.WriteLine("Låten får inte spelas!");
+        }
+    }
+}
